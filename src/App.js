bf04c21c3cefa790420733ddcd3f77feb25ffc0c
Ye Min Htet <yeminhtet@ad.gicjp.com>
2022-06-28 13:50:09 +0900

    mod

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello GIC
+          Hello GIC.ltd
         </p>
         <a
           className="App-link"
